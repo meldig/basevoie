4702a396904617d1b3f4945d1b7351a382d52fdd
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-05-27 11:54:56 +0200

    maj_codes_migration_base_voie

--- sql/scripts/code_ddl_g_base_voie/integration/import_donnees_tables_base_voie.bat
+++ sql/scripts/code_ddl_g_base_voie/integration/import_donnees_tables_base_voie.bat
@@ -4,13 +4,12 @@ echo Bienvenu dans l'import des donnees de la base voie du schema actuel vers le
 :: 1. gestion des identifiants Oracle
 SET /p USER_D="Veuillez saisir l'utilisateur Oracle de destination : "
 SET /p USER_P="Veuillez saisir l'utilisateur Oracle de provenance : "
-SET /p USER_P_AGENT="Veuillez saisir l'utilisateur Oracle de provenance pour les AGENTS : "
 SET /p MDP_D="Veuillez saisir le mot de passe de l'utilisateur Oracle de destination : "
 SET /p MDP_P="Veuillez saisir le mot de passe de l'utilisateur Oracle de provenance : "
-SET /p MDP_P_AGENT="Veuillez saisir le mot de passe de l'utilisateur Oracle de provenance pour les AGENTS : "
 SET /p INSTANCE_D="Veuillez saisir l'instance Oracle de destination: "
 SET /p INSTANCE_P="Veuillez saisir l'instance Oracle de provenance : "
-SET /p INSTANCE_P_AGENT="Veuillez saisir l'instance Oracle de provenance pour les AGENTS : "
+SET /p CHEMIN_AGENT="Veuillez saisir le chemin d'acces au fichier temp_agent : "
+SET /p CHEMIN_FAMILLE_LIBELLE="Veuillez saisir le chemin d'acces au dossier integration : "
 
 :: 2. se mettre dans l'environnement QGIS
 cd C:\Program Files\QGIS 3.16\bin
@@ -23,40 +22,49 @@ setx PROJ_LIB "C:\Program Files\QGIS 3.16\share\proj"
 
 :: 5. commande ogr2ogr pour exporter les couches du schéma X@X vers le schéma X@X
 :: 5.1. table ILTATRC
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.ILTATRC" -nln TEMP_ILTATRC -nlt LINESTRING -lco SRID=2154 -dim 2
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:ILTATRC -sql "SELECT * FROM G_SIDU.ILTATRC" -nln TEMP_ILTATRC -nlt LINESTRING -lco SRID=2154 -dim 2
 
 :: 5.2. table ILTAPTZ
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.ILTAPTZ" -nln TEMP_ILTAPTZ -nlt POINT -lco SRID=2154 -dim 2
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:ILTAPTZ -sql "SELECT * FROM G_SIDU.ILTAPTZ" -nln TEMP_ILTAPTZ -nlt POINT -lco SRID=2154 -dim 2
 
 :: 5.3. table ILTADTN
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.ILTADTN" -nln TEMP_ILTADTN
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:ILTADTN -sql "SELECT * FROM G_SIDU.ILTADTN" -nln TEMP_ILTADTN
 
 :: 5.4. table VOIEVOI
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.VOIEVOI" -nln TEMP_VOIEVOI
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:VOIEVOI -sql "SELECT * FROM G_SIDU.VOIEVOI" -nln TEMP_VOIEVOI
 
 :: 5.5. table VOIECVT
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.VOIECVT" -nln TEMP_VOIECVT
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:VOIECVT -sql "SELECT * FROM G_SIDU.VOIECVT" -nln TEMP_VOIECVT
 
 :: 5.6. table VOIECVT
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.TYPEVOIE" -nln TEMP_TYPEVOIE
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:TYPEVOIE -sql "SELECT * FROM G_SIDU.TYPEVOIE" -nln TEMP_TYPEVOIE
 
 :: 5.7. table VOIECVT
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.ILTASEU" -nln TEMP_ILTASEU -nlt POINT -lco SRID=2154 -dim 2
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:ILTASEU -sql "SELECT * FROM G_SIDU.ILTASEU" -nln TEMP_ILTASEU -nlt POINT -lco SRID=2154 -dim 2
 
 :: 5.8. table VOIECVT
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.ILTASIT" -nln TEMP_ILTASIT
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:ILTASIT -sql "SELECT * FROM G_SIDU.ILTASIT" -nln TEMP_ILTASIT
 
 :: 5.9. table VOIECVT
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.ILTAFILIA" -nln TEMP_ILTAFILIA
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:ILTAFILIA -sql "SELECT * FROM G_SIDU.ILTAFILIA" -nln TEMP_ILTAFILIA
 
 :: 5.10. table VOIECVT
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.TA_RUE" -nln TEMP_TA_RUE
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:TA_RUE -sql "SELECT * FROM G_SIDU.TA_RUE" -nln TEMP_TA_RUE
 
 :: 5.11. table VOIECVT
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.TA_RUEVOIE" -nln TEMP_TA_RUEVOIE
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:TA_RUEVOIE -sql "SELECT * FROM G_SIDU.TA_RUEVOIE" -nln TEMP_TA_RUEVOIE
 
 :: 5.12. table VOIECVT
-ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM G_SIDU.ILTALPU" -nln TEMP_ILTALPU
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P%:ILTALPU -sql "SELECT * FROM G_SIDU.ILTALPU" -nln TEMP_ILTALPU
+
+:: 5.13. table TEMP_AGENT
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% %CHEMIN_AGENT%/TEMP_AGENT.csv
+
+:: 5.14 table TEMP_FAMILLE
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% %CHEMIN_FAMILLE_LIBELLE%/TEMP_FAMILLE.csv
+
+:: 5.15 table TEMP_FAMILLE
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% %CHEMIN_FAMILLE_LIBELLE%/TEMP_LIBELLE.csv
 
 :: 6. MISE EN PAUSE
 PAUSE
