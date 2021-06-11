4b2e30cce953a227b2aa551c5d1a5d805e6b5a0e
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-06-11 16:09:07 +0200

    mise à jour du DDL suite aux modifications du MRD
    - Remplacement de TA_FANTOIR par TA_RIVOLI (+FK, PK, références, etc) ;
    - Suppression du champ TA_VOIE.LATERALITE_COMMUNE ;
    - Suppression des champs dates début/fin de validité ;
    - Suppression des FK présentes dans les tables de log vers les tables principales ;
    - Rassemblement des champs par types afin d'en faciliter la lecture ;

--- sql/scripts/code_ddl_g_base_voie/tables/creation_ta_infos_seuil.sql
+++ sql/scripts/code_ddl_g_base_voie/tables/creation_ta_infos_seuil.sql
@@ -8,10 +8,10 @@ CREATE TABLE G_BASE_VOIE.TA_INFOS_SEUIL(
     numero_seuil NUMBER(5,0) NOT NULL,
     numero_parcelle CHAR(9) NOT NULL,
     complement_numero_seuil VARCHAR2(10),
-    fid_seuil NUMBER(38,0) NOT NULL,
     date_saisie DATE DEFAULT sysdate NOT NULL,
-    fid_pnom_saisie NUMBER(38,0),
     date_modification DATE DEFAULT sysdate NOT NULL,
+    fid_seuil NUMBER(38,0) NOT NULL,
+    fid_pnom_saisie NUMBER(38,0),
     fid_pnom_modification NUMBER(38,0)
 );
 
@@ -21,10 +21,10 @@ COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.objectid IS 'Clé primaire auto-inc
 COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.numero_seuil IS 'Numéro de seuil.';
 COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.numero_parcelle IS 'Numéro de parcelle issu du cadastre.';
 COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.complement_numero_seuil IS 'Complément du numéro de seuil. Exemple : 1 bis';
-COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.fid_seuil IS 'Clé étrangère vers la table TA_SEUIL, permettant d''affecter une géométrie à un ou plusieurs seuils, dans le cas où plusieurs se superposent sur le même point.';
 COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.date_saisie IS 'Date de saisie des informations du seuil (automatique).';
-COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.fid_pnom_saisie IS 'Clé étrangère vers la table TA_AGENT permettant de récupérer le pnom de l''agent ayant créé les informations d''un seuil.';
 COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.date_modification IS 'Date de modification des informations du seuil (via un trigger).';
+COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.fid_seuil IS 'Clé étrangère vers la table TA_SEUIL, permettant d''affecter une géométrie à un ou plusieurs seuils, dans le cas où plusieurs se superposent sur le même point.';
+COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.fid_pnom_saisie IS 'Clé étrangère vers la table TA_AGENT permettant de récupérer le pnom de l''agent ayant créé les informations d''un seuil.';
 COMMENT ON COLUMN G_BASE_VOIE.TA_INFOS_SEUIL.fid_pnom_modification IS 'Clé étrangère vers la table TA_AGENT permettant de récupérer le pnom de l''agent ayant modifié les informations d''un seuil.';
 
 -- 3. Création de la clé primaire
