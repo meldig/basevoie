42fd8ee5c2ca50cc91b74b688a3e6872d05678bc
romain-jault <57357284+romain-jault@users.noreply.github.com>
2021-06-25 15:33:25 +0200

    Update vue_bal_ban_ign_1_2.sql
    Correction de la vue suite à une relecture:
    - champ cle_interop
    - commune
    - parcelle

--- sql/scripts/code_ddl_g_base_voie/vues/vue_bal_ban_ign_1_2.sql
+++ sql/scripts/code_ddl_g_base_voie/vues/vue_bal_ban_ign_1_2.sql
@@ -25,45 +25,57 @@ CREATE OR REPLACE FORCE VIEW "G_BASE_VOIE"."V_BAL_BAN_IGN" (
 CONSTRAINT "PK_V_BAL_BAN_IGN" PRIMARY KEY ("CLE_INTEROP") DISABLE) AS 
 SELECT
     a.OBJECTID AS "UID_ADRESSE",
+-- GESTION DU CHAMP CLE INTEROP SI IL N'Y A PAS DE COMPLEMENT DE SEUIL
     CASE
       WHEN a.complement_numero_seuil IS NULL
-      THEN b.code_insee||'_'|| '591' || h.code_rivoli || substr(b.code_insee,3,5) || h.cle_controle ||'_'||a.numero_seuil
-      ELSE
+        THEN
+          CASE LENGTH(CAST (a.numero_seuil AS VARCHAR(5)))
+            WHEN 0
+              THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '00000'
+            WHEN 1
+              THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '0000' || a.numero_seuil
+            WHEN 2
+              THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '000' || a.numero_seuil
+            WHEN 3
+              THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '00' || a.numero_seuil
+            WHEN 4
+              THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '0' || a.numero_seuil
+            WHEN 5
+              THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| a.numero_seuil
+          END
+    ELSE
+-- GESTION DU CHAMP CLE INTEROP SI IL N'Y A UN COMPLEMENT DE SEUIL
             CASE LENGTH(CAST (a.numero_seuil AS VARCHAR(5)))
+                WHEN 0
+            THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '00000' ||'_'|| LOWER(a.complement_numero_seuil)
                 WHEN 1
-                  THEN b.code_insee||'_'|| '591' || h.code_rivoli || substr(b.code_insee,3,5) || h.cle_controle ||'_'|| '0000' || a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
+                  THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '0000' || a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
                 WHEN 2
-                  THEN b.code_insee||'_'|| '591' || h.code_rivoli || substr(b.code_insee,3,5) || h.cle_controle ||'_'|| '000' || a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
+                  THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '000' || a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
                 WHEN 3
-                  THEN b.code_insee||'_'|| '591' || h.code_rivoli || substr(b.code_insee,3,5) || h.cle_controle ||'_'|| '00' || a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
+                  THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '00' || a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
                 WHEN 4
-                  THEN b.code_insee||'_'|| '591' || h.code_rivoli || substr(b.code_insee,3,5) || h.cle_controle ||'_'|| '0' || a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
+                  THEN b.code_insee||'_'|| h.code_rivoli ||'_'|| '0' || a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
                 ELSE
-                  b.code_insee||'_'|| '591' || h.code_rivoli || substr(b.code_insee,3,5) || h.cle_controle ||'_'|| a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
+                  b.code_insee||'_'|| h.code_rivoli ||'_'|| a.numero_seuil||'_'|| LOWER(a.complement_numero_seuil)
             END
     END AS "CLE_INTEROP",
-    -- COMMUNE_INSEE
-    CASE b.code_insee
-      WHEN '59298'
-        THEN '59350'
-      WHEN '59355'
-        THEN '59350'
+-- COMMUNE_INSEE
+    CASE i.nature
+      WHEN 'Commune associé'
+        THEN j.code_insee
       ELSE b.code_insee
     END AS "COMMUNE_INSEE",
     -- COMMUNE_NOM
-    CASE CAST(b.code_insee AS CHAR)
-      WHEN '59298'
-        THEN 'LILLE'
-      WHEN '59355'
-        THEN 'LILLE'
+    CASE i.nature
+      WHEN 'Commune associé'
+        THEN j.nom
       ELSE i.NOM
     END AS "COMMUNE_NOM",
     -- COMMUNE_DELEGUEE_INSEE
-    CASE b.code_insee
-      WHEN '59298'
-        THEN '59298'
-      WHEN '59355'
-        THEN '59355'
+    CASE i.nature
+      WHEN 'Commune associé'
+        THEN b.code_insee
       ELSE
         NULL
     END AS "COMMUNE_DELEGUEE_INSEE",
@@ -86,19 +98,27 @@ SELECT
   b.geom.sdo_point.y AS "Y",
   SDO_CS.TRANSFORM(b.geom,4326).sdo_point.x AS "LONG",
   SDO_CS.TRANSFORM(b.geom,4326).sdo_point.y AS "LAT",
-  'MEL' SOURCE,
-  a.numero_parcelle AS "CAD_PARCELLES",
+  'MEL' AS SOURCE,
+  -- NUMERO PARCELLE
+  CASE a.numero_parcelle
+    WHEN 'NR'
+      THEN NULL
+    ELSE a.numero_parcelle
+  END AS "CAD_PARCELLES",
   a.date_modification AS "DATE_DER_MAJ"
 FROM
   g_base_voie.ta_infos_seuil a
-INNER JOIN g_base_voie.ta_seuil b ON b.objectid = a.objectid
+INNER JOIN g_base_voie.ta_seuil b ON b.objectid = a.fid_seuil
 INNER JOIN g_base_voie.ta_relation_troncon_seuil c ON c.fid_seuil = b.objectid
 INNER JOIN g_base_voie.ta_troncon d ON d.objectid = c.fid_troncon
 INNER JOIN g_base_voie.ta_relation_troncon_voie e ON e.fid_troncon = d.objectid
 INNER JOIN g_base_voie.ta_voie f ON f.objectid = e.fid_voie
 INNER JOIN g_base_voie.ta_type_voie g ON g.objectid = f.fid_typevoie
 INNER JOIN g_base_voie.ta_rivoli h ON h.objectid = f.fid_rivoli
-INNER JOIN G_REFERENTIEL.A_COMMUNE i ON i.code_insee = b.code_insee
+INNER JOIN G_REFERENTIEL.MEL_COMMUNE_LLH i ON i.code_insee = b.code_insee,
+  G_REFERENTIEL.MEL_COMMUNE j
+WHERE
+  SDO_RELATE(i.geom, j.geom,'mask=OVERLAPBDYINTERSECT') = 'TRUE'
 ;
 
 -- 2. Création des commentaires de table et de colonnes
