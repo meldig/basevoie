19d13133a5f9eefddac63dad6da6573b7c3e2d04
romain-jault <57357284+romain-jault@users.noreply.github.com>
2021-07-05 14:05:55 +0200

    Update V_TRONCON.sql
    Ajout d'une CTE pour éviter les doublons.

--- sql/scripts/code_ddl_g_base_voie/vues/V_TRONCON.sql
+++ sql/scripts/code_ddl_g_base_voie/vues/V_TRONCON.sql
@@ -11,56 +11,67 @@ CREATE OR REPLACE FORCE VIEW V_TRONCON_LITTERALIS (
     LARGEUR,
     GEOMETRY,
     CONSTRAINT "V_TRONCON_LITTERALIS_PK" PRIMARY KEY ("CODE_TRONC") DISABLE)
-    AS (
-            SELECT
-                ROWNUM AS IDENTIFIANT,
-                a.objectid AS CODE_TRONC,
-                CASE 
-                    WHEN e.domania = 'AUTOROUTE OU VOIE A CARACTERE AUTOROUTIER'
-                    THEN 'A'
-                    WHEN e.domania = 'ROUTE NATIONALE'
-                    THEN 'RN' -- Route Nationale
-                    WHEN e.domania IN ('VOIE PRIVEE ENTRETENUE PAR LA CUDL','VOIE PRIVEE FERMEE','VOIE PRIVEE OUVERTE','AUTRE VOIE PRIVEE','DECLASSEMENT EN COURS')
-                    THEN 'VP' -- Voie Privée
-                    WHEN e.domania = 'CHEMIN RURAL'
-                    THEN 'CR' -- Chemin Rural
-                    WHEN e.domania IN ('VOIE METROPOLITAINE','GESTION COMMUNAUTAIRE','AUTRE VOIE PUBLIQUE')
-                    THEN 'VC' -- Voie Communale
-                END AS CLASSEMENT,
-                c.objectid AS CODE_RUE_G,
-                UPPER(d.libelle) || ' ' || UPPER(c.libelle_voie) AS NOM_RUE_G,
-                f.CODE_INSEE AS INSEE_G,
-                c.objectid AS CODE_RUE_D,
-                UPPER(d.libelle) || ' ' || UPPER(c.libelle_voie) AS NOM_RUE_D,
-                f.CODE_INSEE AS INSEE_D,
-                'NULL' AS LARGEUR,
-                a.geom AS GEOMETRY
-            FROM
-                G_BASE_VOIE.TA_TRONCON a
-                INNER JOIN G_BASE_VOIE.TA_RELATION_TRONCON_VOIE b ON b.fid_troncon = a.objectid
-                INNER JOIN G_BASE_VOIE.TA_VOIE c ON c.objectid = b.fid_voie
-                INNER JOIN G_BASE_VOIE.TA_TYPE_VOIE d ON d.objectid = c.fid_typevoie
-                INNER JOIN SIREO_LEC.OUT_DOMANIALITE e ON e.cnumtrc = a.objectid,
-                G_REFERENTIEL.MEL_COMMUNE f,
-                USER_SDO_GEOM_METADATA m
-            WHERE
-            -- Pour rechercher l'INSEE du troncon nous analysons sur quelle commune se situe le point median du troncon.
-		    SDO_CONTAINS(
-		                f.geom,
-		                 SDO_LRS.CONVERT_TO_STD_GEOM(
-		                                            SDO_LRS.LOCATE_PT(
-		                                                            SDO_LRS.CONVERT_TO_LRS_GEOM(a.GEOM,m.diminfo),
-		                                                            SDO_GEOM.SDO_LENGTH(a.GEOM,m.diminfo)/2
-		                                                            )
-		                                             )
-		                )='TRUE'
-            AND
-                m.table_name = 'TA_TRONCON'
-        );
+    AS 
+        WITH C_1 AS(
+                    SELECT DISTINCT
+                        a.objectid AS CODE_TRONC,
+                        CASE 
+                            WHEN e.domania = 'AUTOROUTE OU VOIE A CARACTERE AUTOROUTIER'
+                            THEN 'A'
+                            WHEN e.domania = 'ROUTE NATIONALE'
+                            THEN 'RN' -- Route Nationale
+                            WHEN e.domania IN ('VOIE PRIVEE ENTRETENUE PAR LA CUDL','VOIE PRIVEE FERMEE','VOIE PRIVEE OUVERTE','AUTRE VOIE PRIVEE','DECLASSEMENT EN COURS')
+                            THEN 'VP' -- Voie Privée
+                            WHEN e.domania = 'CHEMIN RURAL'
+                            THEN 'CR' -- Chemin Rural
+                            WHEN e.domania IN ('VOIE METROPOLITAINE','GESTION COMMUNAUTAIRE','AUTRE VOIE PUBLIQUE')
+                            THEN 'VC' -- Voie Communale
+                        END AS CLASSEMENT,
+                        c.objectid AS CODE_RUE_G,
+                        UPPER(d.libelle) || ' ' || UPPER(c.libelle_voie) AS NOM_RUE_G,
+                        c.objectid AS CODE_RUE_D,
+                        UPPER(d.libelle) || ' ' || UPPER(c.libelle_voie) AS NOM_RUE_D
+                    FROM
+                        G_BASE_VOIE.TA_TRONCON a
+                        INNER JOIN G_BASE_VOIE.TA_RELATION_TRONCON_VOIE b ON b.fid_troncon = a.objectid
+                        INNER JOIN G_BASE_VOIE.TA_VOIE c ON c.objectid = b.fid_voie
+                        INNER JOIN G_BASE_VOIE.TA_TYPE_VOIE d ON d.objectid = c.fid_typevoie
+                        INNER JOIN SIREO_LEC.OUT_DOMANIALITE e ON e.cnumtrc = a.objectid
+                )
+                SELECT
+                    ROWNUM AS IDENTIFIANT,
+                    a.CODE_TRONC,
+                    a.CLASSEMENT,
+                    a.CODE_RUE_G,
+                    a.NOM_RUE_G,
+                    f.CODE_INSEE AS INSEE_G,
+                    a.CODE_RUE_D,
+                    a.NOM_RUE_D,
+                    f.CODE_INSEE AS INSEE_D,
+                    'NULL' AS LARGEUR,
+                    b.geom AS GEOMETRY
+                FROM
+                    C_1 a
+                    INNER JOIN G_BASE_VOIE.TA_TRONCON b ON b.objectid = a.code_tronc,
+                    G_REFERENTIEL.MEL_COMMUNE f,
+                    USER_SDO_GEOM_METADATA m
+                WHERE
+                -- Pour rechercher l'INSEE du troncon nous analysons sur quelle commune se situe le point median du troncon.
+                SDO_CONTAINS(
+                            f.geom,
+                             SDO_LRS.CONVERT_TO_STD_GEOM(
+                                                        SDO_LRS.LOCATE_PT(
+                                                                        SDO_LRS.CONVERT_TO_LRS_GEOM(b.GEOM,m.diminfo),
+                                                                        SDO_GEOM.SDO_LENGTH(b.GEOM,m.diminfo)/2
+                                                                        )
+                                                         )
+                            )='TRUE'
+                AND
+                    m.table_name = 'TA_TRONCON';
 
 -- 2. Création des commentaires de la vue
 COMMENT ON TABLE G_BASE_VOIE.V_TRONCON_LITTERALIS IS 'Vue regroupant la liste des tronçons constituant une voie. Chaque objet de cette vue décrit un tronçon de voie LITTERALIS';
-COMMENT ON COLUMN G_BASE_VOIE.V_TRONCON_LITTERALIS.IDENTIFIANT IS "Cle primaire de la vue"
+COMMENT ON COLUMN G_BASE_VOIE.V_TRONCON_LITTERALIS.IDENTIFIANT IS 'Cle primaire de la vue';
 COMMENT ON COLUMN G_BASE_VOIE.V_TRONCON_LITTERALIS.CODE_TRONC IS 'Identificateur unique et immuable du tronçon de voie partagé entre Littéralis Expert et le SIG.';
 COMMENT ON COLUMN G_BASE_VOIE.V_TRONCON_LITTERALIS.CLASSEMENT IS 'Classement de la voie.';
 COMMENT ON COLUMN G_BASE_VOIE.V_TRONCON_LITTERALIS.CODE_RUE_G IS 'Code unique de la rue côté gauche du tronçon partagé entre Littéralis Expert et le SIG.';
