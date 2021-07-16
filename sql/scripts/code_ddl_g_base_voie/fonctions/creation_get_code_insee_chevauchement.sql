5b1ce98f1f611a314f42a06853fed88a66eb04d5
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-07-16 16:50:02 +0200

    correction des points d'interets
    - création d'une relation N:N en vue d'une possible complication des points d'intérêt (un point géométrique peut disposer de plusieurs points d'intérêts).

---
+++ sql/scripts/code_ddl_g_base_voie/fonctions/creation_get_code_insee_chevauchement.sql
@@ -0,0 +1,48 @@
+/*
+la fonction ci-dessous a pour objectif de retouver le code insee de la commune dans laquelle se trouve un objet pouvant chevaucher plusieurs communes.
+*/
+
+create or replace FUNCTION GET_CODE_INSEE_CHEVAUCHEMENT(v_table_name VARCHAR2, v_geometry SDO_GEOMETRY) RETURN CHAR
+/*
+Cette fonction a pour objectif de récupérer le code insse de la commune dans laquelle se situe un objet géométrique (contenu, chevauchant ou intersectant).
+ATTENTION : Cette fonction N'EST PAS A UTILISER pour des objets de types points. Pour ce cas veuillez utiliser la fonction GET_CODE_INSEE_CONTENU.
+*/
+-- Fonction test servant à tester les fonctions basées sur des index
+    DETERMINISTIC
+    As
+    v_code_insee CHAR(8);
+    BEGIN
+        -- Sélection du code insee de la commune dans laquelle le centroïde de l''entité se situe
+        SELECT
+            b.code_insee INTO v_code_insee
+        FROM
+            G_REFERENTIEL.MEL_COMMUNE b,
+            USER_SDO_GEOM_METADATA m
+         WHERE
+            m.table_name = v_table_name
+            AND SDO_CONTAINS(
+                    b.geom,
+                    SDO_LRS.CONVERT_TO_STD_GEOM(
+                        SDO_LRS.LOCATE_PT(
+                                        SDO_LRS.CONVERT_TO_LRS_GEOM(v_geometry,m.diminfo),
+                                        SDO_GEOM.SDO_LENGTH(v_geometry,m.diminfo)/2
+                        )
+                    )
+                )='TRUE';
+
+        IF v_code_insee IS NULL THEN -- Pour les lignes ou polygones situés à la frontière de la MEL et dont le centroïde peut être en-dehors de la métropole.
+            SELECT
+                b.code_insee INTO v_code_insee
+            FROM
+                G_REFERENTIEL.MEL_COMMUNE b,
+            WHERE
+                SDO_ANYINTERACT(v_geometry, b.geom) = 'TRUE';
+        END IF;
+
+        RETURN v_code_insee;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+             RETURN 'error';
+    END GET_CODE_INSEE_CHEVAUCHEMENT;
+
+/
