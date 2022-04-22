97f2b8dfcfa4158dc7956a62f6d0c6758d612e3d
romain-jault <57357284+romain-jault@users.noreply.github.com>
2022-04-22 16:28:43 +0200

    Correction VM et adaptation VM_AUDIT_ERREUR_BASE_VOIE
    Correction de vues, mise ajour de la vue VM_AUDIT_ERREUR_BASE_VOIE suite à la création des vues matérialisées.

--- sql/scripts/code_dml/audit_base_voie/vm_audit_erreur_base_voie.sql
+++ sql/scripts/code_dml/audit_base_voie/vm_audit_erreur_base_voie.sql
@@ -14,98 +14,100 @@ WITH CTE_1 AS
 	    COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre d''adresses en doublons de numéro, côté de la voie, complément de numéro de seuil, identifiant de voie et distance seuil/tronçon.' AS type_erreur
 	FROM
-	    G_BASE_VOIE.V_AUDIT_DOUBLON_SEUIL_NUMERO_COMPLEMENT_VOIE_DISTANCE a
+	    G_BASE_VOIE.VM_AUDIT_DOUBLON_SEUIL_NUMERO_COMPLEMENT_VOIE_DISTANCE a
 	UNION ALL
-	SELECT
+/*	SELECT
 	    COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de seuils dont la distance par rapport à leur tronçon d''affectation est supérieure à 1000m.' AS type_erreur
 	FROM
-	    G_BASE_VOIE.V_AUDIT_DISTANCE_SEUIL_TRONCON_SUPERIEURE_1KM a
+	    G_BASE_VOIE.VM_AUDIT_DISTANCE_SEUIL_TRONCON_SUPERIEURE_1KM a
 	UNION ALL
+*/
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Adresses en doublons de numéro, côté de la voie, complément de numéro de seuil et identifiant de voie.' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_DOUBLON_SEUIL_NUMERO_COMPLEMENT_VOIE a
+		G_BASE_VOIE.VM_AUDIT_DOUBLON_SEUIL_NUMERO_COMPLEMENT_VOIE a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Troncons qui ne s''intersectent pas sur les points de depart ni d''arrivé' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_INTERSECTION_HORS_POINTS_DEPART_ARRIVE a
+		G_BASE_VOIE.VM_AUDIT_INTERSECTION_HORS_POINTS_DEPART_ARRIVE a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de seuils et les troncons qui s''intersectent.' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_INTERSECTION_SEUIL_TRONCON a
+		G_BASE_VOIE.VM_AUDIT_INTERSECTION_SEUIL_TRONCON a
 	UNION ALL
-	SELECT
+/*	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Seuils distant de moins de 50 cm.' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_SEUIL_DISTANCE_50_CM a
+		G_BASE_VOIE.VM_AUDIT_SEUIL_DISTANCE_50_CM a
 	UNION ALL
+*/
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Seuils situés hors MEL suite au changement de référentiel commune' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_SEUIL_HORS_MEL a
+		G_BASE_VOIE.VM_AUDIT_SEUIL_HORS_MEL a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Troncons situés hors MEL suite au changement de référentiel commune' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_TRONCON_HORS_MEL a
+		G_BASE_VOIE.VM_AUDIT_TRONCON_HORS_MEL a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Troncons d''une meme voie qui ne s''intersectent pas sur les points de depart ni d''arrivé' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF a
+		G_BASE_VOIE.VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de troncon affectes à plusieurs voies' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_TRONCON_PLUSIEURS_VOIES a
+		G_BASE_VOIE.VM_AUDIT_TRONCON_PLUSIEURS_VOIES a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de troncon qui ne n''ont pas de domanialite (absent de la table SIREO_LEC.OUT_DOMANIALITE)' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_TRONCON_SANS_DOMANIALITE a
+		G_BASE_VOIE.VM_AUDIT_TRONCON_SANS_DOMANIALITE a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de type de voies présent dans la table voievoi mais absent de la table typevoie.' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_TYPE_VOIE_DANS_VOIEVOI_MAIS_ABSENT_TYPE_VOIE a
+		G_BASE_VOIE.VM_AUDIT_TYPE_VOIE_DANS_VOIEVOI_MAIS_ABSENT_TYPE_VOIE a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de types de voies de la table typevoie sans libelle.' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_TYPE_VOIE_SANS_LITY_VOIE a
+		G_BASE_VOIE.VM_AUDIT_TYPE_VOIE_SANS_LITY_VOIE a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de voie secondaire affectées à plusiseurs voie' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_VOIE_SECONDAIRE_AVEC_PLUSIEURS_VOIE_PRINCIPALES a
+		G_BASE_VOIE.VM_AUDIT_VOIE_SECONDAIRE_AVEC_PLUSIEURS_VOIE_PRINCIPALES a
 	UNION ALL
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de voie sans nom' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_VOIEVOIE_CNOMINUS_NULL a
+		G_BASE_VOIE.VM_AUDIT_VOIEVOIE_CNOMINUS_NULL a
 	UNION ALL
 
 	SELECT
 		COUNT(a.identifiant) AS nombre_d_erreur,
 		'Nombre de voie dont le genre est NULL' AS type_erreur
 	FROM
-		G_BASE_VOIE.V_AUDIT_VOIEVOIE_GENRE_NULL a
+		G_BASE_VOIE.VM_AUDIT_VOIEVOIE_GENRE_NULL a
 	)
 SELECT
 	rownum AS identifiant,
