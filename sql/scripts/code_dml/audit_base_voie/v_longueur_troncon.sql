ab316c29778c1369a41fdcec1b36b4ca2fed7720
romain-jault <57357284+romain-jault@users.noreply.github.com>
2022-05-06 16:11:03 +0200

    Mise à jour des vues
    Ajout de la vue: v_longeurs_troncon.sql
    Mise à jour des vues matérialisées:
    - vm_audit_troncon_meme_voie_non_jointif
    - vm_audit_troncon_meme_voie_non_jointif_plusieurs_metre.sql
    - vm_audit_troncon_non_jointif.sql

---
+++ sql/scripts/code_dml/audit_base_voie/v_longueur_troncon.sql
@@ -0,0 +1,26 @@
+-- V_LONGUEUR_TRONCON: longueur des troncons
+
+-- 1. Création de la vue
+CREATE OR REPLACE FORCE VIEW V_LONGUEUR_TRONCON(identifiant,longueur, geom,
+CONSTRAINT "V_LONGUEUR_TRONCON_PK" PRIMARY KEY ("IDENTIFIANT") DISABLE) AS
+SELECT
+    cnumtrc AS numero_troncon,
+    SDO_LRS.GEOM_SEGMENT_LENGTH
+    (
+     ora_geometry
+    ),
+    ora_geometry
+FROM
+    G_BASE_VOIE.TEMP_ILTATRC
+WHERE
+    CDVALTRO = 'V'
+;
+
+
+-- 2. Commentaire de la vue.
+COMMENT ON TABLE G_BASE_VOIE.V_LONGUEUR_TRONCON  IS 'Vue qui présente la longueur des troncons';
+
+-- 3. Commentaire des colonnes
+COMMENT ON COLUMN G_BASE_VOIE.V_LONGUEUR_TRONCON.IDENTIFIANT IS 'Clé primaire de la vue, numero du troncon.';
+COMMENT ON COLUMN G_BASE_VOIE.V_LONGUEUR_TRONCON.LONGUEUR IS 'longueur du troncon.';
+COMMENT ON COLUMN G_BASE_VOIE.V_LONGUEUR_TRONCON.GEOM IS 'Géométrie du troncon de type ligne.';
--- sql/scripts/code_dml/audit_base_voie/vm_audit_troncon_meme_voie_non_jointif.sql
+++ sql/scripts/code_dml/audit_base_voie/vm_audit_troncon_meme_voie_non_jointif.sql
@@ -4,20 +4,15 @@
 -- DROP MATERIALIZED VIEW VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF;
 
 -- 1. Creation vue
-CREATE MATERIALIZED VIEW G_BASE_VOIE.VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF (IDENTIFIANT, CODE_TRONCON_A, CODE_TRONCON_B, STATUT_CONNECTION)
+CREATE MATERIALIZED VIEW G_BASE_VOIE.VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF (IDENTIFIANT, CODE_TRONCON_A, CODE_TRONCON_B)
 REFRESH ON DEMAND
 FORCE
 DISABLE QUERY REWRITE
 AS
 SELECT
-    rownum as identifiant,
-    a.cnumtrc AS code_troncon_a,
-    d.cnumtrc AS code_troncon_b,
-    SDO_LRS.CONNECTED_GEOM_SEGMENTS(
-        SDO_LRS.CONVERT_TO_LRS_GEOM(a.ora_geometry),
-        SDO_LRS.CONVERT_TO_LRS_GEOM(d.ora_geometry),
-        0.005
-    ) AS statut_connection
+    ROWNUM,
+    a.cnumtrc,
+    d.cnumtrc
 FROM
     G_BASE_VOIE.TEMP_ILTATRC a
     INNER JOIN G_BASE_VOIE.TEMP_VOIECVT b ON b.cnumtrc = a.cnumtrc
@@ -25,19 +20,8 @@ FROM
     G_BASE_VOIE.TEMP_ILTATRC d
     INNER JOIN G_BASE_VOIE.TEMP_VOIECVT e ON e.cnumtrc = d.cnumtrc
     INNER JOIN G_BASE_VOIE.TEMP_VOIEVOI f ON f.ccomvoi = e.ccomvoi
-WHERE 
-    a.cnumtrc <> d.cnumtrc
-    AND a.cnumtrc < d.cnumtrc
-    AND SDO_WITHIN_DISTANCE(
-            d.ora_geometry, 
-            a.ora_geometry, 
-            'distance = 0.5'
-        ) = 'TRUE'
-    AND SDO_LRS.CONNECTED_GEOM_SEGMENTS(
-            SDO_LRS.CONVERT_TO_LRS_GEOM(a.ora_geometry), 
-            SDO_LRS.CONVERT_TO_LRS_GEOM(d.ora_geometry), 
-            0.005
-        ) = 'FALSE'
+WHERE
+    a.cnumtrc < d.cnumtrc
     AND b.ccomvoi = e.ccomvoi
     AND a.cdvaltro = 'V'
     AND b.cvalide = 'V'
@@ -45,7 +29,11 @@ WHERE
     AND d.cdvaltro = 'V'
     AND e.cvalide = 'V'
     AND f.cdvalvoi = 'V'
-;
+    AND SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.ora_geometry, 0.005) = 'TRUE'
+    AND SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(d.ora_geometry, 0.005) = 'TRUE'
+    AND SDO_WITHIN_DISTANCE(a.ora_geometry, d.ora_geometry, 'distance = 0.5') = 'TRUE'
+    AND SDO_ANYINTERACT(a.ora_geometry, d.ora_geometry) <> 'TRUE'
+    ;
 
 
 -- 2. Clé primaire
@@ -60,4 +48,3 @@ COMMENT ON MATERIALIZED VIEW G_BASE_VOIE.VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF 
 COMMENT ON COLUMN G_BASE_VOIE.VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF.IDENTIFIANT IS 'Clé primaire de la vue.';
 COMMENT ON COLUMN G_BASE_VOIE.VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF.code_troncon_a IS 'Identifiant du troncon considéré a.';
 COMMENT ON COLUMN G_BASE_VOIE.VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF.code_troncon_b IS 'Identifiant du troncon considéré b.';
-COMMENT ON COLUMN G_BASE_VOIE.VM_AUDIT_TRONCON_MEME_VOIE_NON_JOINTIF.statut_connection IS 'Statut de la connection.';
