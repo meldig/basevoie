0505b5dbf05299d75a73f2cf95bbd7dfa32922a0
JACQ Baptiste <50438172+BaptisteJacq@users.noreply.github.com>
2022-06-08 16:11:26 +0200

    ajout de tous les codes nécessaires au projet A
    - Code DDL des objets du projet A en base ;
    - Fichiers bat permettant la compilation et le lancement des codes ;
    - Codes DML permettant le remplissage de la structure du projet A ;
    - Codes de suppression des objets du projet A ;

---
+++ sql/scripts/code_ddl_g_base_voie/integration/correction_base_voie/suppression_structures_transitoires/suppression_structure_projet_a.sql
@@ -0,0 +1,37 @@
+/*
+Requêtes sql permettant de supprimer les tables de la base voie, servant à tester la structure tenant compte de la latéralité des voies, avec leur MTD spatiale, index, contraintes, triggers et fonctions.
+*/
+
+-- 1. Suppression des tables
+DROP TABLE G_BASE_VOIE.TEMP_A_TRONCON CASCADE CONSTRAINTS;
+DROP TABLE G_BASE_VOIE.TEMP_A_VOIE_PHYSIQUE CASCADE CONSTRAINTS;
+DROP TABLE G_BASE_VOIE.TEMP_A_VOIE_ADMINISTRATIVE CASCADE CONSTRAINTS;
+DROP TABLE G_BASE_VOIE.TEMP_A_LIBELLE CASCADE CONSTRAINTS;
+DROP TABLE G_BASE_VOIE.TEMP_A_TYPE_VOIE CASCADE CONSTRAINTS;
+DROP TABLE G_BASE_VOIE.TEMP_A_AGENT CASCADE CONSTRAINTS;
+
+-- 2. Suppression des vues
+DROP VIEW G_BASE_VOIE.V_TEMP_VOIE_AGREGEE_SENS_CIRCULATION_RECTIFIE;
+
+-- 3. Suppression des vues matérialisées
+DROP MATERIALIZED VIEW G_BASE_VOIE.VM_TEMP_IMPORT_VOIE_AGREGEE;
+
+-- 4. Suppression des métadonnées spatiales
+DELETE FROM USER_SDO_GEOM_METADATA
+WHERE TABLE_NAME = 'TEMP_A_TRONCON';
+
+DELETE FROM USER_SDO_GEOM_METADATA
+WHERE TABLE_NAME = 'VM_TEMP_IMPORT_VOIE_AGREGEE';
+
+DELETE FROM USER_SDO_GEOM_METADATA
+WHERE TABLE_NAME = 'V_TEMP_VOIE_AGREGEE_SENS_CIRCULATION_RECTIFIE';
+
+-- 5. Suppression des séquences
+DROP SEQUENCE SEQ_TEMP_A_TRONCON_OBJECTID;
+
+-- 6. Suppression des fonctions
+DROP FUNCTION get_temp_code_insee_97_communes_contain_line;
+DROP FUNCTION get_temp_code_insee_97_communes_pourcentage;
+DROP FUNCTION get_temp_code_insee_97_communes_within_distance;
+DROP FUNCTION get_temp_code_insee_97_communes_troncon;
+COMMIT;
---
+++ sql/scripts/code_ddl_g_base_voie/sequences/creation_seq_temp_a_troncon_objectid.sql
@@ -0,0 +1,8 @@
+/*
+SEQ_TEMP_A_TRONCON_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TEMP_A_TRONCON
+*/
+
+CREATE SEQUENCE SEQ_TEMP_A_TRONCON_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
