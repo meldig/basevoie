324c4f553584048e5a69942df1d8151c9ae39963
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2022-12-06 10:32:25 +0100

    correction du trigger B_UXX_TEMP_C_VOIE_ADMINISTRATIVE_PRINCIPALE_MATERIALISE
    Le trigger pointait vers la mauvaise table TA_AGENT, donc les agents ne pouvaient pas enregistrer leurs vérifications.

--- sql/scripts/code_ddl_g_base_voie/declencheurs/creation_b_uxx_temp_c_voie_administrative_principale_materialise.sql
+++ sql/scripts/code_ddl_g_base_voie/declencheurs/creation_b_uxx_temp_c_voie_administrative_principale_materialise.sql
@@ -11,7 +11,7 @@ DECLARE
 BEGIN
     -- Sélection du pnom
     SELECT sys_context('USERENV','OS_USER') into username from dual;
-    SELECT numero_agent INTO v_id_agent FROM G_BASE_VOIE.TA_AGENT WHERE pnom = username;
+    SELECT numero_agent INTO v_id_agent FROM G_BASE_VOIE.TEMP_C_AGENT WHERE pnom = username;
        
     UPDATE G_BASE_VOIE.TEMP_C_VOIE_ADMINISTRATIVE a
         SET a.libelle_voie = :new.nom_voie, a.date_modification = TO_DATE(sysdate, 'dd/mm/yy'), a.fid_pnom_modification = v_id_agent, a.commentaire = :new.commentaire, a.complement_nom_voie = :new.complement_nom_voie, a.fid_type_voie = :new.fid_type_voie
