372bec18ab2ca9a907a321b88169bb55df51c2c6
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2022-12-13 16:58:43 +0100

    correction du code à la marge
    - mise à jour du trigger

--- sql/scripts/code_ddl_g_base_voie/declencheurs/creation_b_iux_temp_g_troncon_date_pnom.sql
+++ sql/scripts/code_ddl_g_base_voie/declencheurs/creation_b_iux_temp_g_troncon_date_pnom.sql
@@ -9,16 +9,12 @@ FOR EACH ROW
 DECLARE
     username VARCHAR2(100);
     v_id_agent NUMBER(38,0);
-    v_id_etat NUMBER(38,0);
 BEGIN
     -- Sélection du pnom
     SELECT sys_context('USERENV','OS_USER') into username from dual;
 
     -- Sélection de l'id du pnom correspondant dans la table TEMP_AGENT
     SELECT numero_agent INTO v_id_agent FROM G_BASE_VOIE.TEMP_G_AGENT WHERE pnom = username;
-
-    -- Sélection du libellé caractérisant une nouvelle entité
-    SELECT objectid INTO v_id_etat FROM G_BASE_VOIE.TEMP_G_LIBELLE WHERE UPPER(libelle_court) = UPPER('nouvelle entité');
 
     -- En cas d'insertion on insère la FK du pnom de l'agent, ayant créé le tronçon, présent dans TEMP_AGENT. 
     IF INSERTING THEN 
@@ -27,7 +23,6 @@ BEGIN
         :new.date_saisie := TO_DATE(sysdate, 'dd/mm/yy');
         :new.fid_pnom_modification := v_id_agent;
         :new.date_modification := TO_DATE(sysdate, 'dd/mm/yy');    
-        :new.fid_etat := v_id_etat;
     ELSE
         -- En cas de mise à jour on édite le champ date_modification avec la date du jour et le champ fid_pnom_modification avec la FK du pnom de l'agent, ayant modifié le tronçon, présent dans TEMP_AGENT.
         IF UPDATING THEN 
@@ -40,7 +35,6 @@ BEGIN
         WHEN OTHERS THEN
             mail.sendmail('bjacq@lillemetropole.fr',SQLERRM,'ERREUR TRIGGER - G_BASE_VOIE.B_IUX_TEMP_G_TRONCON_DATE_PNOM','bjacq@lillemetropole.fr');
 END;
-
 
 /
 
