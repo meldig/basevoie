4fe1622a55002f27fdeeca61a029555db3bc736f
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2023-01-09 17:06:42 +0100

    Création de tous les codes de la structure du projet h de correction
    Mise à jour des relations seuils/tronçons

---
+++ sql/scripts/code_ddl_g_base_voie/sequences/creation_seq_temp_h_troncon_objectid.sql
@@ -0,0 +1,8 @@
+/*
+SEQ_TEMP_H_TRONCON_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TEMP_H_TRONCON du projet H de correction des relations tronçons/seuils
+*/
+
+CREATE SEQUENCE SEQ_TEMP_H_TRONCON_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
