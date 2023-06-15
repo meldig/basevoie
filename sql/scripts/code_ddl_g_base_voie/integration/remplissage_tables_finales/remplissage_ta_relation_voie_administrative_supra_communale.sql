85a9ca0f1999dc87438d803113bf69808b5dc6a8
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2023-06-15 17:17:12 +0200

    création d'une relation tronçons/voies supra-communale
    demandeur : Ulrich
    objectif : lui montrer que c'est possible
    
    reste à faire :
    - demander de préciser son besoin et son usage

--- sql/scripts/code_ddl_g_base_voie/integration/remplissage_tables_finales/remplissage_ta_relation_voie_administrative_supra_communale.sql
+++ sql/scripts/code_ddl_g_base_voie/integration/remplissage_tables_finales/remplissage_ta_relation_voie_administrative_supra_communale.sql
@@ -4,17 +4,54 @@ entre les voies supra-communales et les voies administratives.
 */
 MERGE INTO G_BASE_VOIE.TA_RELATION_VOIE_ADMINISTRATIVE_SUPRA_COMMUNALE a 
     USING(
-        SELECT DISTINCT
-            a.idsupvoi AS id_voie_supra_communale,
-            f.objectid AS id_voie_administrative
-        FROM
-            SIREO_LEC.EXRD_IDSUPVOIE a 
-            INNER JOIN SIREO_LEC.OUT_DOMANIALITE b ON b.idvoie = a.idsupvoi
-            INNER JOIN G_BASE_VOIE.TA_TRONCON c ON c.old_objectid = b.cnumtrc
-            INNER JOIN G_BASE_VOIE.TA_VOIE_PHYSIQUE d ON d.objectid = c.fid_voie_physique
-            INNER JOIN G_BASE_VOIE.TA_RELATION_VOIE_PHYSIQUE_ADMINISTRATIVE e ON e.fid_voie_physique = d.objectid
-            INNER JOIN G_BASE_VOIE.TA_VOIE_ADMINISTRATIVE f ON f.objectid = e.fid_voie_administrative
-            
+        WITH
+            C_1 AS(
+                SELECT DISTINCT -- Sélection des voies supra-communales absentes de la table EXRD_IDSUPVOIE
+                    a.idvoie AS id_voie_supra_communale
+                FROM
+                    SIREO_LEC.OUT_DOMANIALITE a 
+                    INNER JOIN G_BASE_VOIE.TA_TRONCON b ON b.old_objectid = a.cnumtrc
+                    INNER JOIN G_BASE_VOIE.TA_VOIE_PHYSIQUE c ON c.objectid = b.fid_voie_physique
+                    INNER JOIN G_BASE_VOIE.TA_RELATION_VOIE_PHYSIQUE_ADMINISTRATIVE d ON d.fid_voie_physique = c.objectid
+                    INNER JOIN G_BASE_VOIE.TA_VOIE_ADMINISTRATIVE e ON e.objectid = d.fid_voie_administrative
+                WHERE
+                    a.idvoie NOT IN(SELECT idsupvoi FROM SIREO_LEC.EXRD_IDSUPVOIE)
+                GROUP BY
+                    a.idvoie
+                HAVING
+                    COUNT(DISTINCT e.code_insee) > 1
+            ),
+
+            C_2 AS (
+                SELECT DISTINCT -- Sélection des relations voies administratives/supra-communales hors EXRD
+                    a.idvoie AS id_voie_supra_communale,
+                    e.objectid AS id_voie_administrative
+                FROM
+                    SIREO_LEC.OUT_DOMANIALITE a 
+                    INNER JOIN G_BASE_VOIE.TA_TRONCON b ON b.old_objectid = a.cnumtrc
+                    INNER JOIN G_BASE_VOIE.TA_VOIE_PHYSIQUE c ON c.objectid = b.fid_voie_physique
+                    INNER JOIN G_BASE_VOIE.TA_RELATION_VOIE_PHYSIQUE_ADMINISTRATIVE d ON d.fid_voie_physique = c.objectid
+                    INNER JOIN G_BASE_VOIE.TA_VOIE_ADMINISTRATIVE e ON e.objectid = d.fid_voie_administrative
+                WHERE
+                    a.idvoie IN (SELECT id_voie_supra_communale FROM C_1)
+                UNION ALL
+                SELECT DISTINCT -- Sélection des relations voies administratives/supra-communales présentes dans la table EXRD_IDSUPVOIE
+                    a.idsupvoi AS id_voie_supra_communale,
+                    f.objectid AS id_voie_administrative
+                FROM
+                    SIREO_LEC.EXRD_IDSUPVOIE a 
+                    INNER JOIN SIREO_LEC.OUT_DOMANIALITE b ON b.idvoie = a.idsupvoi
+                    INNER JOIN G_BASE_VOIE.TA_TRONCON c ON c.old_objectid = b.cnumtrc
+                    INNER JOIN G_BASE_VOIE.TA_VOIE_PHYSIQUE d ON d.objectid = c.fid_voie_physique
+                    INNER JOIN G_BASE_VOIE.TA_RELATION_VOIE_PHYSIQUE_ADMINISTRATIVE e ON e.fid_voie_physique = d.objectid
+                    INNER JOIN G_BASE_VOIE.TA_VOIE_ADMINISTRATIVE f ON f.objectid = e.fid_voie_administrative       
+            )
+
+            SELECT DISTINCT
+                id_voie_supra_communale,
+                id_voie_administrative
+            FROM 
+                C_2            
     )t 
 ON(a.fid_voie_supra_communale = t.id_voie_supra_communale AND a.fid_voie_administrative = t.id_voie_administrative)
 WHEN NOT MATCHED THEN
