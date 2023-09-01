6d4fcaf23d54a63c3f271a7b4de0134eb2242f5b
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2023-09-01 09:44:51 +0200

    coquille

--- sql/scripts/code_ddl_g_base_voie/integration/litteralis/temp_code_ddl_travaux_projet_litteralis.sql
+++ sql/scripts/code_ddl_g_base_voie/integration/litteralis/temp_code_ddl_travaux_projet_litteralis.sql
@@ -6,7 +6,7 @@ BEGIN
     DBMS_SCHEDULER.CREATE_JOB (
    job_name          =>  'JOB_MAJ_VM_TAMPON_LITTERALIS_VOIE_ADMINISTRATIVE',
    job_type          =>  'PLSQL_BLOCK',
-   job_action        =>  'DBMS_REFRESH.REFRESH('"G_BASE_VOIE"."VM_TAMPON_LITTERALIS_VOIE_ADMINISTRATIVE"');', 
+   job_action        =>  'DBMS_REFRESH.REFRESH(''"G_BASE_VOIE"."VM_TAMPON_LITTERALIS_VOIE_ADMINISTRATIVE"'');', 
    start_date        =>  '02/07/23 07:00:00 EUROPE/PARIS',
    repeat_interval   =>  'FREQ=MONTHLY; INTERVAL=1; BYDAY=SAT',
    comments          =>  'Ce job rafraîchit la VM G_BASE_VOIE.VM_TAMPON_LITTERALIS_VOIE_ADMINISTRATIVE le premier dimanche du mois à 07h00.');
